feee00e3f0d3c820ad8775715b35668fb05c7df2
Pieter Huibers <pieterhuibers@gmail.com>
2016-12-30 14:35:29 +0100

    Allow user to supply the colors and numbers as commabd line parameter

--- 17.py
+++ 17.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
 
-characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z']
+r_nums = [1,4,7,10,13,16,19,22]
+g_nums = [2,5,8,11,14,20,23,26]
+b_nums = [6,9,12,15,18,21]
 min_nr = 1
 max_nr = 26
 
@@ -24,44 +27,47 @@ def to_word(word) :
 	result = ''
 	for c in word : 
 		result = result + chr(c+96)
-	return result
-
-
-def complete(word, char_index, colors, sums) :
-	for c in range(1,26) :
-		word[char_index] = c
-		if(char_index == len(word)-1) :
-			if(is_valid(word,colors,sums)) :
-					print(to_word(word))
-		else :
-			complete(word, char_index+1, colors, sums)
-
-
-def combinations(colors, sums) :
-	characters = len(colors)
-	word = np.repeat(0,characters)
-	complete(word, 0, colors, sums)
 
 def create_word(c_red, c_green, c_blue, colors) :
 	i_red = 0
 	i_green = 0
 	i_blue = 0
 	word = ''
+	numbers = '['
 	for c in colors :
 		if(c == 'r') :
 			word = word + chr(c_red[i_red] + 96)
+			numbers = numbers + str(c_red[i_red]) + ','
 			i_red += 1
 		if(c == 'g') :
 			word = word + chr(c_green[i_green] + 96)
+			numbers = numbers + str(c_green[i_green]) + ','
 			i_green += 1
 		if(c == 'b') :
 			word = word + chr(c_blue[i_blue] + 96)
+			numbers = numbers + str(c_blue[i_blue]) + ','
 			i_blue += 1
-	return word
+	return word+','+numbers[0:-1] + ']'
 
-def main():
-	colors = ['r','g','g','b','r','g','r','b','b','b','r','r']
-	sums = [56,30,60]
+def find_sums(total, parts, options) :
+		if(parts==0) :
+			return [[]]
+		if(parts==1) :
+			if(total > max_nr or not(total in options)) :
+				return []
+			else :
+				return [[total]]
+		sums = []
+		for i in options :
+			if(i < total) :
+				results = find_sums(total-i, parts-1, options)
+				for result in results :
+					combination = [i]
+					combination.extend(result)
+					sums.append(combination)
+		return sums
+
+def check_colors(colors, sums) :
 	parts = [0,0,0]
 	for i in range(len(colors)) :
 		if(colors[i] == 'r') :
@@ -71,34 +77,25 @@ def main():
 		if(colors[i] == 'b') :
 			parts[2] += 1
 
-	combinations_red = find_sums(sums[0],parts[0])
-	combinations_green = find_sums(sums[1],parts[1])
-	combinations_blue = find_sums(sums[2],parts[2])
-	print(len(combinations_red))
-	print(len(combinations_green))
-	print(len(combinations_blue))
+	combinations_red = find_sums(sums[0],parts[0], r_nums)
+	combinations_green = find_sums(sums[1],parts[1], g_nums)
+	combinations_blue = find_sums(sums[2],parts[2], b_nums)
+	total_combinations = len(combinations_red)*len(combinations_green) * len(combinations_blue)
+	print total_combinations
 	for c_red in combinations_red :
 		for c_green in combinations_green :
 			for c_blue in combinations_blue :
 				print(create_word(c_red, c_green, c_blue, colors))
-				# print(c_red, c_green, c_blue)
 
-def find_sums(total, parts) :
-		if(parts==0) :
-			return [[]]
-		if(parts==1) :
-			if(total > max_nr) :
-				return []
-			else :
-				return [[total]]
-		sums = []
-		for i in range(min_nr, min(total,max_nr+1)) :
-			results = find_sums(total-i, parts-1)
-			for result in results :
-				combination = [i]
-				combination.extend(result)
-				sums.append(combination)
-		return sums
+def main(args):
+	i = 0
+	colors = args[0:-3]
+	sums = [0,0,0]
+	sums[0] = int(args[-3])
+	sums[1] = int(args[-2])
+	sums[2] = int(args[-1])
+	check_colors(colors, sums)
 
 if __name__ == "__main__":
-	main()
+	if len(sys.argv) > 1 :
+		main(sys.argv[1:])
